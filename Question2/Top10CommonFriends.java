99f54d0360efdcf1e907b176ccb739ef297baec0
Mythri Thippareddy <mythri.thippareddy@gmail.com>
2018-10-24 05:27:11 -0500

    Updating reducer class

--- Question2/Top10CommonFriends.java
+++ Question2/Top10CommonFriends.java
@@ -2,31 +2,24 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.GenericOptionsParser;
 
-public class StatesOfMutualFriends {
-	static int input_friend1;
-	static int input_friend2;
-	static HashMap<String, String> userMap;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+
+public class Top10CommonFriends {
 
 	public static class MapperClass1 extends Mapper<LongWritable, Text, Text, Text> {
 
 		public void map(LongWritable key, Text values, Context context) throws IOException, InterruptedException {
-			Configuration conf_1 = context.getConfiguration();
-			input_friend1 = Integer.parseInt(conf_1.get("InputFriend1"));
-			input_friend2 = Integer.parseInt(conf_1.get("InputFriend2"));
 
 			String[] friends = values.toString().split("\t");
 
@@ -38,152 +31,113 @@ public class StatesOfMutualFriends {
 				Text tuple_key = new Text();
 				for (String friend2 : friendsList) {
 					friend2ID = Integer.parseInt(friend2);
-
-					if ((friend1ID == input_friend1 && friend2ID == input_friend2) || (friend1ID == input_friend2 && friend2ID == input_friend1)) {
-						if (friend1ID < friend2ID) {
-							tuple_key.set(friend1ID + "," + friend2ID);
-						} else {
-							tuple_key.set(friend2ID + "," + friend1ID);
-						}
-						context.write(tuple_key, new Text(friends[1]));
+					if (friend1ID < friend2ID) {
+						tuple_key.set(friend1ID + "," + friend2ID);
+					} else {
+						tuple_key.set(friend2ID + "," + friend1ID);
 					}
+					context.write(tuple_key, new Text(friends[1]));
 				}
 			}
-		}		
+		}
 	}
 
-	public static class ReducerClass1 extends Reducer<Text, Text, Text, Text> {
+	public static class ReducerClass1 extends Reducer<Text, Text, Text, IntWritable> {
+
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 			HashMap<String, Integer> friendsHashMap = new HashMap<String, Integer>();
-			StringBuilder commonFriendLine = new StringBuilder();
-			Text commonFriend = new Text();
+			int NumberOfCommonFriends = 0;
 			for (Text tuples : values) {
 				String[] friendsList = tuples.toString().split(",");
 				for (String eachFriend : friendsList) {
 					if (friendsHashMap.containsKey(eachFriend)) {
-						commonFriendLine.append(eachFriend + ",");
+						NumberOfCommonFriends++;
 					} else {
 						friendsHashMap.put(eachFriend, 1);
 					}
 				}
 			}
-			if (commonFriendLine.length() > 0) {
-				commonFriendLine.deleteCharAt(commonFriendLine.length() - 1);
-			}
-			commonFriend.set(new Text(commonFriendLine.toString()));
-			context.write(key, commonFriend);
+			context.write(key, new IntWritable(NumberOfCommonFriends));
 		}
 	}
 
-	public static class MapperClass2 extends Mapper<LongWritable, Text, Text, Text> {
+	public static class MapperClass2 extends Mapper<Text, Text, LongWritable, Text> {
 
-		protected void setup(Context context) throws IOException, InterruptedException {
-			super.setup(context);
-			Configuration conf = context.getConfiguration();
-			Path part=new Path("/Mythri/input/userdata.txt");//Location of file in HDFS			
-			
-			FileSystem fs = FileSystem.get(conf);
-			FileStatus[] fss = fs.listStatus(part);
+		private LongWritable count = new LongWritable();
 
-			userMap = new HashMap<String, String>();
-			String line;
+		public void map(Text key, Text values, Context context) throws IOException, InterruptedException {
+			int newVal = Integer.parseInt(values.toString());
+			count.set(newVal);
+			context.write(count, key);
+		}
+	}
 
-			for (FileStatus status : fss) {
-				Path pt = status.getPath();
+	public static class ReducerClass2 extends Reducer<LongWritable, Text, Text, LongWritable> {
+		private int idx = 0;
 
-				BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(pt)));
-				line = br.readLine();
-				while(line!=null) {
-					String [] info = line.split(",");
-					if(info.length==10) {
-						userMap.put(info[0], info[1]+":"+info[5]);
-					}
-					line = br.readLine();
+		public void reduce(LongWritable key, Iterable<Text> values, Context context)
+				throws IOException, InterruptedException {
+			for (Text value : values) {
+				if (idx < 10) {
+					idx++;
+					context.write(value, key);
 				}
-
 			}
-		}
-
-		public void map(LongWritable key, Text values, Context context) throws IOException, InterruptedException {
-			System.out.println("This is the Mapper function");
-			String[] info = values.toString().split("\t");
-			String [] mutualFriends = info[1].split(",");
-			for (String friend : mutualFriends) {
-				context.write(new Text(info[0]), new Text(userMap.get(friend)));
-			}			
 		}
 	}
-
-	public static class ReducerClass2 extends Reducer<Text, Text, Text, Text> {
-
-		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-			StringBuilder sb = new StringBuilder();
-			sb.append("[");
-			for (Text tuple : values) {
-				sb.append(tuple.toString()+",");
-			}
-			if (sb.length() > 0) {
-				sb.deleteCharAt(sb.length() - 1);
-			}
-			sb.append("]");
-			context.write(key, new Text(sb.toString()));			
-		}
-	}
-
-
 
 	public static void main(String[] args) throws Exception {
-		Configuration conf1 = new Configuration();
-		String[] otherArgs = new GenericOptionsParser(conf1, args).getRemainingArgs();
-		if (otherArgs.length != 6) {
-					System.out.println("Usage: <Common friends input file path> <user_data file path> <temp_path> <output_path> <User-ID1> <User-ID2> ");
-					System.exit(1);
-				}
+		if (args.length != 3) {
+			System.out.println("Usuage: <Common friends input file path> <temp file path> <output_path>");
+			System.exit(1);
+		}
 
-				conf1.set("InputFriend1", otherArgs[4]);
-				conf1.set("InputFriend2", otherArgs[5]);
+		{
+			Configuration conf1 = new Configuration();
+			Job job1 = Job.getInstance(conf1, "Mutual Friends");
 
-				Job job1 = Job.getInstance(conf1, "Mutual-Friends of userA and userB");
+			job1.setJarByClass(Top10CommonFriends.class);
+			job1.setMapperClass(MapperClass1.class);
+			job1.setReducerClass(ReducerClass1.class);
 
-				job1.setJarByClass(StatesOfMutualFriends.class);
-				job1.setMapperClass(MapperClass1.class);
-				job1.setReducerClass(ReducerClass1.class);
+			job1.setMapOutputKeyClass(Text.class);
+			job1.setMapOutputValueClass(Text.class);
 
-				job1.setMapOutputKeyClass(Text.class);
-				job1.setMapOutputValueClass(Text.class);
+			job1.setOutputKeyClass(Text.class);
+			job1.setOutputValueClass(IntWritable.class);
 
-				job1.setOutputKeyClass(Text.class);
-				job1.setOutputValueClass(Text.class);
+			FileInputFormat.addInputPath(job1, new Path(args[0]));
+			FileOutputFormat.setOutputPath(job1, new Path(args[1]));
 
-				FileInputFormat.addInputPath(job1, new Path(otherArgs[0]));
-				FileOutputFormat.setOutputPath(job1, new Path(otherArgs[2]));
+			if (!job1.waitForCompletion(true)) {
+				System.exit(1);
+			}
 
-				if (!job1.waitForCompletion(true)) {
-					System.exit(1);
-				}
-
+			{
 				Configuration conf2 = new Configuration();
 				Job job2 = Job.getInstance(conf2, "Top 10");
 
-				job2.setJarByClass(StatesOfMutualFriends.class);
+				job2.setJarByClass(Top10CommonFriends.class);
 				job2.setMapperClass(MapperClass2.class);
 				job2.setReducerClass(ReducerClass2.class);
 
-				conf2.set("Data", otherArgs[1]);
-				System.out.println("Driver : Path :"+otherArgs[1]);
-				
-				job2.setMapOutputKeyClass(Text.class);
+				job2.setMapOutputKeyClass(LongWritable.class);
 				job2.setMapOutputValueClass(Text.class);
 
 				job2.setOutputKeyClass(Text.class);
-				job2.setOutputValueClass(Text.class);
-				
-				FileInputFormat.addInputPath(job2, new Path(otherArgs[2]));
-				FileOutputFormat.setOutputPath(job2, new Path(otherArgs[3]));
-				
-				if (!job2.waitForCompletion(true)) {
-					System.exit(1);
-				}
-	}
+				job2.setOutputValueClass(LongWritable.class);
 
+				job2.setInputFormatClass(KeyValueTextInputFormat.class);
+
+				job2.setSortComparatorClass(LongWritable.DecreasingComparator.class);
+
+				job2.setNumReduceTasks(1);
+
+				FileInputFormat.addInputPath(job2, new Path(args[1]));
+				FileOutputFormat.setOutputPath(job2, new Path(args[2]));
+
+				System.exit(job2.waitForCompletion(true) ? 0 : 1);
+			}
+		}
+	}
 }
